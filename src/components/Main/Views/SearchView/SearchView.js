f62511da7083d9effd85951263cc821a1fe3a97f
Damian Kubecki <damian_094@wp.pl>
2022-03-31 21:30:14 +0200

    currencies info updated

--- src/components/Main/Views/SearchView/SearchView.js
+++ src/components/Main/Views/SearchView/SearchView.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React from 'react'
 import styles from './SearchView.module.scss'
 
-const SearchView = () => ( 
-    <h1>Szukaj Waluty</h1>
-)
+class SearchView extends React.Component {
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Szukaj waluty</h2>
+        <p className={styles.paragraph}>Waluta:</p>
+        <p className={styles.paragraph}>Ilość ostatnich notowań:</p>
+      </div>
+    )
+  }
+}
 
 export default SearchView
