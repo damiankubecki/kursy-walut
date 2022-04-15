d22be35c72ce00a48d2bcd54087766868bbc4262
Damian Kubecki <damian_094@wp.pl>
2022-04-15 19:39:10 +0200

    Button component refactored

--- src/components/elements/Button/Button.js
+++ src/components/elements/Button/Button.js
@@ -2,23 +2,31 @@ import React from 'react'
 import styles from './Button.module.scss'
 
 const Button = ({ href, bigger, noBorder, margin, children, ...props }) => {
-  const btnStyle = bigger ? styles.btnBig : styles.btn
   const border = noBorder ? 'none' : '2px solid'
+  const fontSize = bigger ? '1.3rem' : '1rem'
   return (
     <>
       {href ? (
         <a
-          className={btnStyle}
+          className={styles.btn}
           href={href}
-          style={{ borderBottom: border, margin: margin }}
+          style={{
+            borderBottom: border,
+            margin: margin,
+            fontSize: fontSize,
+          }}
           {...props}
         >
           {children}
         </a>
       ) : (
         <button
-          className={btnStyle}
-          style={{ borderBottom: border, margin: margin }}
+          className={styles.btn}
+          style={{
+            borderBottom: border,
+            margin: margin,
+            fontSize: fontSize,
+          }}
           {...props}
         >
           {children}
