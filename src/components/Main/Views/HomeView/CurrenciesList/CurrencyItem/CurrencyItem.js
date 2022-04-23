edf2ffb0fa755f90e9347a4f7623492ebfc3f303
Damian Kubecki <damian_094@wp.pl>
2022-04-23 12:22:01 +0200

    Change HomeView/CurrenciesList styles

--- src/components/Main/Views/HomeView/CurrenciesList/CurrencyItem/CurrencyItem.js
+++ src/components/Main/Views/HomeView/CurrenciesList/CurrencyItem/CurrencyItem.js
@@ -6,27 +6,20 @@ const { darkBlackColor } = variables
 
 const CurrencyItem = ({ currency, code, mid, flag, wikipediaLink }) => {
   return (
-    <div className={styles.wrapper}>
-      {wikipediaLink ? (
-        <a className={styles.link} href={wikipediaLink}>
-          <p className={styles.name}>{currency}</p>
-          <p className={styles.code}>({code})</p>
-        </a>
-      ) : (
-        <>
-          <p className={styles.name}>{currency}</p>
-          <p className={styles.code}>({code})</p>
-        </>
-      )}
-      <p className={styles.mid}>{mid.toFixed(4)}</p>
-      <div
-        className={styles.flag}
-        style={{
-          backgroundImage: `url(${flag || null})`,
-          backgroundColor: darkBlackColor,
-        }}
-      ></div>
-    </div>
+    <a className={styles.link} href={wikipediaLink || null}>
+      <div className={styles.wrapper}>
+        <p className={styles.code}>{code}</p>
+        <p className={styles.mid}>{mid.toFixed(4)}</p>
+        <p className={styles.name}>{currency}</p>
+        <div
+          className={styles.flag}
+          style={{
+            backgroundImage: `url(${flag || null})`,
+            backgroundColor: darkBlackColor,
+          }}
+        ></div>
+      </div>
+    </a>
   )
 }
 
