c4dac27000c67cbb0f7d3780957e673826fa3c3b
Damian Kubecki <damian_094@wp.pl>
2022-04-29 15:57:46 +0200

    Update curiosites collection

--- src/config.js
+++ src/config.js
@@ -7,12 +7,15 @@ const calculatorViewConfig = {
 }
 const curiositesViewConfig = {
   collection: [
-    'Sasin to nowa oraz najbardziej stabilna waluta. Jeden Sasin jest (i będzie) równy 70000000zł.',
-    'Robert Lewandowski zarabia około 1,3 Sasin rocznie.',
-    'W 2020 roku TVP dostało od rządu 28,5 Sasina.',
-    'Cena nowego Poloneza Caro w latach 90 zaczynała się od 0.0003128571429 Sasina.',
-    'Minimalne wynagrodzenie dla umów cywilnoprawynch w 2021 roku wynosi 0.0000002614286 Sasina/h.',
-    'Prognozy przewidują, że w 2050 roku Sasinami będzie można płacić na całym świecie.',
+    'Na Kubie w obiegu znajdą się dwie różne waluty o zupełnie innej wartości.',
+    'Cena nowego Poloneza Caro w latach 90 zaczynała się od 0,0003128571429 Sasina.',
+    'W Tajlandi za porwanie banknotu z wizerunkiem króla, czy też jego przydepnięcie można nawet trafić do więzienia.',
+    'Zyskującym popularność trendem jest produkcja banknotów plastikowych. Wytworzone z polimeru pieniądze cechują się wyższą żywotnością.',
+    'Kostaryka nazwę swoje waluty "colóna" zawdzięcza Krzysztofowi Kolumbowi. Colón to po hiszpańsku bowiem Kolumb.',
+    'Najmniejszy banknot Księgi Rekordów Guinnessa funkcjonuje w Rumunii od 1917 roku. Jest to 10 banów, a jego wymiary to zaledwie 2,75 x 3,8 centymetra.',
+    'Robert Lewandowski zarabia około 1,54 Sasin rocznie.',
+    '14 państw nienależących do UE ma jako walutę narodową euro.',
+    'Na Węgrzech po zakończeniu II wojny światowej największym nominałem był banknot 100 000 000 000 000 000 000 pengő czyli sto trylionów, a inflacja wynosiła 41.9 biliarda % w skali miesiąca, co oznaczało podwajanie się cen przeciętnie co każde 15 godzin.',
   ],
   writingTime: 50,
 }
